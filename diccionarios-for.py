add19b99162622f7bcf8459ffaa4c1fddb111659
Jonathan Perez <jonathanperezvoulquin@gmail.com>
2021-09-07 19:52:18 -0300

    Dictionaries

---
+++ diccionarios-for.py
@@ -0,0 +1,50 @@
+
+
+
+"""
+day_list = {
+	'lun' : 'lunes',
+	'mar' : 'martes',
+	'Mie' :	'miércoles'
+
+}
+for d in day_list:
+	print(d,':', day_list[d])
+"""
+#for d in day_list:
+#	print(d)
+"""
+day_list2 = ['Jose','hector','pedro']
+print(random.choice(day_list2))
+
+for dia in {"lun:lunes", "mar:martes", "mier:miércoles"}:	
+	print(dia)
+print()	
+
+mi_conjunto = {1, 3, 2, 9, 3, 1}
+for e in mi_conjunto:
+    print(e)
+
+diccionario = {'nombre' : 'Carlos', 'edad' : 22, 'cursos': ['Python','Django','JavaScript'] }
+print(diccionario)
+for d in diccionario:
+	print(d, ":", diccionario[d])
+"""
+
+dicc2 = {
+	"vie": 'viernes',
+	"sab": 'sábado',
+}
+
+print(dicc2)
+for i in dicc2:
+	print(i)
+
+for i in dicc2:	
+	print(f"{i}:{dicc2[i]}")
+
+# Esto imprime "Hola, Juan!"
+nombre = "Juan"
+print("Hola, %s!" % nombre)
+
+
